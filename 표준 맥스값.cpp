9c8b87ad669e2742ee637550b0ca06fb998184cc
ldg0005 <ldg0005@gmail.com>
2020-11-23 20:06:48 +0900

    Update 표준 맥스값.cpp

--- 표준 맥스값.cpp
+++ 표준 맥스값.cpp
@@ -1,2 +1,6 @@
 int64_t i64Max = std::numeric_limits<int64_t>::max();
 double f64Min = std::numeric_limits<double>::min();;
+
+std::numeric_limits<double>::lowest();
+std::numeric_limits<double>::infinity();
+std::numeric_limits<double>::quiet_NaN();
